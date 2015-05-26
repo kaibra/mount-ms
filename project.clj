f3821d79e518da2b332938025c84c62d41358ab4
Christian Betz <christian.betz@performance-media.de>
2015-05-26 11:08:38 +0200

    updated dependencies (using output from lein-ancient)

--- project.clj
+++ project.clj
@@ -6,36 +6,36 @@
             :scm { :name "git"
       	           :url "https://github.com/gorillalabs/tesla-microservice"}
             :dependencies [[org.clojure/clojure "1.6.0"]
-                           [com.stuartsierra/component "0.2.2"]
-                           [org.clojure/core.cache "0.6.4"]
-                           [clojurewerkz/propertied "1.2.0"]
-                           [org.clojure/data.json "0.2.5"]
+                           [com.stuartsierra/component "0.2.3"]
+                           [org.clojure/data.json "0.2.6"]
                            [beckon "0.1.1"]
                            [overtone/at-at "1.2.0"]
                            [environ "1.0.0"]
-                           [clj-time "0.8.0"]
+                           [clj-time "0.9.0"]
 
                            [de.otto/status "0.1.0"]
 
                            ;; io
-                           [ring "1.3.1"]
-                           [compojure "1.2.0"]
-                           [hiccup "1.0.5"]
-                           [metrics-clojure "2.3.0"]
-                           [metrics-clojure-graphite "2.3.0"]
+                           [ring "1.3.2"]
+                           [compojure "1.3.4"]
+                           [metrics-clojure "2.5.1"]
+                           [metrics-clojure-graphite "2.5.1"]
 
                            ;; logging
-                           [org.clojure/tools.logging "0.3.0"]
+                           [org.clojure/tools.logging "0.3.1"]
                            ]
 
-            :plugins [[lein-marginalia "0.8.0"]
-                      [lein-environ "1.0.0"]]
             :profiles {:test {:env {:metering-reporter "console"
                                     :import-products   "false"
                                     :server-port       "9991"
                                     :cache-dir         "/tmp"}
                               :resource-paths ["test-resources"]
                               :dependencies [[ring-mock "0.1.5"]
-                                             [http-kit.fake "0.2.1"]]
+                                             [http-kit.fake "0.2.2"]]
                               }
+                       :dev {:plugins [[lein-ancient "0.5.4"]
+                                       [lein-marginalia "0.8.0"]
+                                       [lein-environ "1.0.0"]]
+
+                             }
                        :uberjar {:aot :all}})
