70b560b1ddd1de2b394caa46d2e7f3d7e607cb7d
Kai Brandes <kai.brandes@otto.de>
2016-02-26 18:18:46 +0100

    small fix for old names in require-block

--- src/kaibra/system.clj
+++ src/kaibra/system.clj
@@ -3,17 +3,14 @@
     [beckon :as beckon]
     [clojure.tools.logging :as log]
     [mount.core :as mnt]
-    [kaibra.transition.configuring :as tconf]
-    [kaibra.transition.health :as thealth]
-
+    [kaibra.stateful.health :refer [health] :as hlth]
+    [kaibra.stateful.configuring :refer [config] :as conf]
     [kaibra.stateful.metering :refer [metering]]
     [kaibra.stateful.keep-alive :refer [keep-alive]]
-    [kaibra.stateful.health :refer [health]]
-    [kaibra.stateful.app-status :refer [app-status]]
-    [kaibra.stateful.configuring :refer [config]]))
+    [kaibra.stateful.app-status :refer [app-status]]))
 
 (defn wait! []
-  (if-let [^String wait-time (tconf/conf-prop :wait-ms-on-stop)]
+  (if-let [^String wait-time (conf/conf-prop :wait-ms-on-stop)]
     (try
       (log/info "<- Waiting " wait-time " milliseconds.")
       (Thread/sleep (Integer. wait-time))
@@ -23,12 +20,13 @@
 (defn stop []
   (beckon/reinit-all!)
   (log/info "<- System will be stopped. Setting lock.")
-  (thealth/lock-application)
+  (hlth/lock-application)
   (wait!)
   (log/info "<- Stopping system.")
   (mnt/stop))
 
 (defn start []
+  (log/info "-> Starting the MOUNT-MS base system")
   (mnt/start)
   (doseq [sig ["INT" "TERM"]]
     (reset! (beckon/signal-atom sig) #{stop})))
