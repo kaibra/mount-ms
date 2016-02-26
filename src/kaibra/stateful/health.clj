c5b27135fa9c947fa43fd61ebe6eaf9bcf9de6f8
Kai Brandes <kai.brandes@otto.de>
2016-02-26 17:23:52 +0100

    merged transition and stateful for health

--- src/kaibra/stateful/health.clj
+++ src/kaibra/stateful/health.clj
@@ -1,10 +1,32 @@
 (ns kaibra.stateful.health
   (:require [compojure.core :as c]
             [clojure.tools.logging :as log]
-            [mount.core :refer [defstate]]))
+            [mount.core :refer [defstate]]
+            [kaibra.stateful.configuring :as conf]))
 
 (defn start-health []
   (log/info "-> Starting healthcheck.")
   {:locked (atom false)})
 
 (defstate health :start (start-health))
+
+;; http response for a healthy system
+(def healthy-response {:status  200
+                       :headers {"Content-Type" "text/plain"}
+                       :body    "HEALTHY"})
+;; http response for an unhealthy system
+(def unhealthy-response {:status  503
+                         :headers {"Content-Type" "text/plain"}
+                         :body    "UNHEALTHY"})
+
+(defn health-response [self]
+  (if @(:locked self)
+    unhealthy-response
+    healthy-response))
+
+(defn health-handler []
+  (let [health-path (or (conf/conf-prop :health-url) "/health")]
+    (c/routes (c/GET health-path [] (health-response health)))))
+
+(defn lock-application []
+  (reset! (:locked health) true))
--- test/kaibra/stateful/health_test.clj
+++ test/kaibra/stateful/health_test.clj
@@ -1,20 +1,21 @@
-(ns kaibra.transition.health-test
-  (:require [clojure.test :refer :all]
-            [kaibra.transition.health :as thealth]
-            [kaibra.util.test-utils :as u]
-            [ring.mock.request :as mock]))
+(ns kaibra.stateful.health-test
+  (:require
+    [clojure.test :refer :all]
+    [kaibra.stateful.health :as health]
+    [ring.mock.request :as mock]
+    [kaibra.util.test-utils :as u]))
 
 (deftest ^:unit should-turn-unhealthy-when-locked
   (u/with-started-system
     (testing "it is still healthy when not yet locked"
-      (let [response ((thealth/health-handler) (mock/request :get "/health"))]
+      (let [response ((health/health-handler) (mock/request :get "/health"))]
         (are [key value] (= value (get response key))
                          :body "HEALTHY"
                          :status 200)))
 
     (testing "when locked, it is unhealthy"
-      (thealth/lock-application)
-      (let [response ((thealth/health-handler) (mock/request :get "/health"))]
+      (health/lock-application)
+      (let [response ((health/health-handler) (mock/request :get "/health"))]
         (are [key value] (= value (get response key))
                          :body "UNHEALTHY"
                          :status 503)))))
@@ -22,12 +23,12 @@
 (deftest ^:integration should-serve-health-under-configured-url
   (testing "use the default url"
     (u/with-started-system
-      (is (= (:body ((thealth/health-handler) (mock/request :get "/health")))
+      (is (= (:body ((health/health-handler) (mock/request :get "/health")))
              "HEALTHY"))))
 
   (testing "use the configuration url"
     (u/with-runtime-config
       {:health-url "/my-health"}
       (u/with-started-system
-        (is (= (:body ((thealth/health-handler) (mock/request :get "/my-health")))
+        (is (= (:body ((health/health-handler) (mock/request :get "/my-health")))
                "HEALTHY"))))))
