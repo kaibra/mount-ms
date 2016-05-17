05433b4c4206a8f23b38c558f975c5a77c386c49
Dr. Christian Betz <cb.betz@gmail.com>
2016-05-17 22:03:36 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs/tesla "0.4.3-SNAPSHOT"
+(defproject gorillalabs/tesla "0.4.4-SNAPSHOT"
   :description "basic microservice."
   :plugins [[lein-modules "0.3.11"]]
   :modules {:dirs       ["core/" "components/mongo" "components/titan/" "components/quartzite/"]
