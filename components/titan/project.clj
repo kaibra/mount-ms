e73af965f9c949f0e66aa4e626de29eb20a85e5c
Dr. Christian Betz <cb.betz@gmail.com>
2016-05-19 14:56:29 +0200

    Version 0.4.7

--- components/titan/project.clj
+++ components/titan/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/titan "0.4.7-SNAPSHOT"
+(defproject gorillalabs.tesla/titan "0.4.7"
             :plugins [[lein-modules "0.3.11"]]
             :modules {:parent "../.."}
             :description "A titan component"
