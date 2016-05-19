1200b6a8ae9f085d8bef2ddfe8c67a0ceee1d9b6
Dr. Christian Betz <cb.betz@gmail.com>
2016-05-19 14:57:19 +0200

    Version 0.4.8-SNAPSHOT

--- components/mongo/project.clj
+++ components/mongo/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/mongo "0.4.7"
+(defproject gorillalabs.tesla/mongo "0.4.8-SNAPSHOT"
             :plugins [[lein-modules "0.3.11"]]
             :modules {:parent "../.."}
             :description "A mongo component"
--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/core "0.4.7"
+(defproject gorillalabs.tesla/core "0.4.8-SNAPSHOT"
             :plugins [[lein-modules "0.3.11"]]
             :description "basic microservice."
             :dependencies [[mount "0.1.10"]
