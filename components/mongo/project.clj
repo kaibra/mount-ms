0854a6e49117e28c1c11fd8ffc58dd81b060e5ce
Dr. Christian Betz <cb.betz@gmail.com>
2016-05-19 14:48:03 +0200

    Version 0.4.6

--- components/mongo/project.clj
+++ components/mongo/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/mongo "0.4.6-SNAPSHOT"
+(defproject gorillalabs.tesla/mongo "0.4.6"
             :plugins [[lein-modules "0.3.11"]]
             :modules {:parent "../.."}
             :description "A mongo component"
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs/tesla "0.4.6-SNAPSHOT"
+(defproject gorillalabs/tesla "0.4.6"
   :description "basic microservice."
   :plugins [[lein-modules "0.3.11"]]
   :modules {:dirs       ["core/" "components/mongo" "components/titan/" "components/quartzite/"]
