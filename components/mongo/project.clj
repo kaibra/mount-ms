2d1a0ed13ddabbf220a096d3e0869d4dde870372
Roland Kaercher <ich@rolandkaercher.de>
2016-05-31 11:06:34 +0200

    Version 0.4.8

--- components/mongo/project.clj
+++ components/mongo/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/mongo "0.4.8-SNAPSHOT"
+(defproject gorillalabs.tesla/mongo "0.4.8"
             :plugins [[lein-modules "0.3.11"]]
             :modules {:parent "../.."}
             :description "A mongo component"
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs/tesla "0.4.8-SNAPSHOT"
+(defproject gorillalabs/tesla "0.4.8"
   :description "basic microservice."
   :plugins [[lein-modules "0.3.11"]
             [lein-pprint "1.1.1"]]
