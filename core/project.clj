ebe0aff02cf5fe4991a8cf6dd314fe4069fcd98f
Dr. Christian Betz <cb.betz@gmail.com>
2016-06-13 13:33:09 +0200

    Version 0.4.10-SNAPSHOT

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/core "0.4.9"
+(defproject gorillalabs.tesla/core "0.4.10-SNAPSHOT"
             :plugins [[lein-modules "0.3.11"]]
             :description "basic microservice."
             :dependencies [[mount "0.1.10"]
