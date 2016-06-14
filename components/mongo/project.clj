e9608aeca994b4333108497227a95793beeef3d0
Dr. Christian Betz <cb.betz@gmail.com>
2016-06-14 12:39:10 +0200

    Version 0.4.12-SNAPSHOT

--- components/mongo/project.clj
+++ components/mongo/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/mongo "0.4.11"
+(defproject gorillalabs.tesla/mongo "0.4.12-SNAPSHOT"
             :plugins [[lein-modules "0.3.11"]]
             :modules {:parent "../.."}
             :description "A mongo component"
