2a4567140cf183d5f1a86b1ac213d633210c90c2
Dr. Christian Betz <cb.betz@gmail.com>
2016-06-14 10:57:44 +0200

    Version 0.4.10

--- components/quartzite/project.clj
+++ components/quartzite/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/quartzite "0.4.10-SNAPSHOT"
+(defproject gorillalabs.tesla/quartzite "0.4.10"
             :plugins [[lein-modules "0.3.11"]]
             :modules {:parent "../.."}
             :description "A quartzite component"
