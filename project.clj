e5dba3088aff04aecc9ef0a1aa9e8a4090947e96
Bjoern Petri <bpetri@gmx.net>
2016-07-19 09:45:58 +0200

    Add Sente component to project.clj

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "basic microservice."
   :plugins [[lein-modules "0.3.11"]
             [lein-pprint "1.1.1"]]
-  :modules {:dirs       ["core/" "components/mongo" "components/titan/" "components/quartzite/"]
+  :modules {:dirs       ["core/" "components/mongo" "components/titan/" "components/quartzite/" "components/sente/"]
             :subprocess nil
             :inherited  {:url                 "https://github.com/gorillalabs/tesla"
                          :license             {:name "Apache License 2.0"
