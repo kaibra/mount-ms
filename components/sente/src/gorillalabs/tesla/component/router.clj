7d67ae7db5c468cec3f704230d2ca2f80684470e
Bjoern Petri <bpetri@gmx.net>
2016-10-12 19:40:29 +0200

    Use core.async to allow concurrent event msg handling

--- components/sente/src/gorillalabs/tesla/component/router.clj
+++ components/sente/src/gorillalabs/tesla/component/router.clj
@@ -1,6 +1,7 @@
 ( ns gorillalabs.tesla.component.router
   (:require [mount.core :as mnt]
             [clojure.tools.logging :as log]
+            [clojure.core.async :as async :refer (<! <!! >! >!! put! chan go go-loop close!)]
             [gorillalabs.tesla.component.socket :as socket]
             [taoensso.sente :as sente]))
 
@@ -8,27 +9,33 @@
 
 (defn- event-msg-handler
   "Wraps `-event-msg-handler` with logging, error catching, etc."
-  [{:as ev-msg :keys [id ?data event ?reply-fn]}]
-
-  (if-let [event-fn (get @(:events router) id)]
-    (event-fn ev-msg)
-    (do (log/info "Unhandled event: %s" id)
-        (when ?reply-fn
-          (?reply-fn {:umatched-event-as-echoed-from-from-server event})))))
+  [event-msg-channel]
+  (go-loop [event-msg (<! event-msg-channel)]
+           (when event-msg
+             (let [{:keys [id event ?reply-fn]} event-msg]
+               (go (if-let [event-fn (get @(:events router) id)]
+                     (event-fn event-msg)
+                     (do (log/info "Unhandled event: %s" id)
+                         (when ?reply-fn
+                           (?reply-fn {:umatched-event-as-echoed-from-from-server event}))))))
+             (recur (<! event-msg-channel)))))
 
 (defn register [router-component id callback]
   (swap! (:events router-component) assoc id callback))
 
 (defn deregister [router-component id]
-  (swap! (:events  router-component) dissoc id))
+  (swap! (:events router-component) dissoc id))
 
 (defn- start []
   (log/info "-> Starting sente router")
-  {:events (atom {}) :router (sente/start-server-chsk-router!  (:ch-recv socket/socket) event-msg-handler) })
+  (let [event-msg-channel (chan)]
+    (event-msg-handler event-msg-channel)
+    {:event-msg-channel event-msg-channel :events (atom {}) :router (sente/start-server-chsk-router! (:ch-recv socket/socket) #(go (>! event-msg-channel %)))}))
 
 (defn- stop [self]
   (log/info "<- Stopping sente router.")
-  (when (:router self) ((:router self))))
+  (when (:router self) ((:router self)))
+  (when (:event-msg-channel self) (close! (:event-msg-channel self))))
 
 (mnt/defstate router
   :start (start)
