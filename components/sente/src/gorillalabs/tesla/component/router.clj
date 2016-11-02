a3c2184a743bac8bda6849baa8e8a4cf5f0a6875
Christoph <git@xrstf.de>
2016-11-02 21:06:47 +0100

    [router] let middlewares take care of handling exceptions, simplify (de)register API

--- components/sente/src/gorillalabs/tesla/component/router.clj
+++ components/sente/src/gorillalabs/tesla/component/router.clj
@@ -11,34 +11,47 @@
   "Wraps `-event-msg-handler` with logging, error catching, etc."
   [event-msg-channel]
   (go-loop [event-msg (<! event-msg-channel)]
-           (when event-msg
-             (let [{:keys [id event ?reply-fn]} event-msg]
-               (go (try
-                     (if-let [event-fn (get @(:events router) id)]
-                       (event-fn event-msg)
-                       (do (log/infof "Unhandled event: %s" id)
-                           (when ?reply-fn
-                             (?reply-fn {:umatched-event-as-echoed-from-from-server event}))))
-                     (catch Exception e (log/error  "Caught exception: " (.getMessage e))))
-                   ))
-             (recur (<! event-msg-channel)))))
+    (when event-msg
+      (let [{:keys [id event ?reply-fn]} event-msg]
+        (go (if-let [event-fn (get @(:events router) id)]
+              (event-fn event-msg)
+              (do (log/infof "Unhandled event: %s" id)
+                  (when ?reply-fn
+                    (?reply-fn {:umatched-event event})))))))
+    (recur (<! event-msg-channel))))
 
-(defn register [router-component id callback]
-  (swap! (:events router-component) assoc id callback))
+(defn register
+  ([id callback]
+   (register router id callback))
+  ([router-component id callback]
+   (swap! (:events router-component) assoc id callback)))
 
-(defn deregister [router-component id]
-  (swap! (:events router-component) dissoc id))
+(defn deregister
+  ([id]
+   (deregister router id))
+  ([router-component id]
+   (swap! (:events router-component) dissoc id)))
+
+;; This is the bridge between incoming HTTP requests and our our
+;; little event channel, from which we then further dispatch the
+;; then-called "events" to their handlers.
+(defn- move-request-to-queue [request]
+  ;; (clojure.pprint/pprint "here comes the request")
+  ;; (clojure.pprint/pprint request)
+  (go (>! (:event-msg-channel router) request)))
 
 (defn- start []
   (log/info "-> Starting sente router.")
   (let [event-msg-channel (chan)]
     (event-msg-handler event-msg-channel)
-    {:event-msg-channel event-msg-channel :events (atom {}) :router (sente/start-server-chsk-router! (:ch-recv socket/socket) #(go (>! event-msg-channel %)))}))
+    {:event-msg-channel event-msg-channel
+     :events (atom {})
+     :router (sente/start-server-chsk-router! (:ch-recv socket/socket) move-request-to-queue)}))
 
 (defn- stop [self]
   (log/info "<- Stopping sente router.")
-  (when (:router self) ((:router self)))
-  (when (:event-msg-channel self) (close! (:event-msg-channel self))))
+  (when-let [router (:router self)] (router))
+  (when-let [channel (:event-msg-channel self)] (close! channel)))
 
 (mnt/defstate router
   :start (start)
