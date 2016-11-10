a96ffe3282e0ba870b20f321c6649292e03ee2aa
Roland <ich@rolandkaercher.de>
2016-11-10 15:40:54 +0100

    Updated titan deps.

--- components/titan/project.clj
+++ components/titan/project.clj
@@ -3,9 +3,9 @@
             :modules {:parent "../.."}
             :description "A titan component"
             :dependencies [[gorillalabs.tesla/core :version]
-                           [gorillalabs/titanium "1.0.2"]
-                           [gorillalabs/titan-cassandra "1.1.0g"]
-                           [gorillalabs/titan-lucene "1.1.0g"]
+                           [gorillalabs/titanium "1.0.5"]
+                           [gorillalabs/titan-cassandra "1.1.1"]
+                           [gorillalabs/titan-lucene "1.1.1"]
                            [org.xerial.snappy/snappy-java "1.1.2.4"] ;; keep this version
                            [commons-codec "1.10"]  ;; keep this version
                            [commons-lang "2.6"]  ;; keep this version
