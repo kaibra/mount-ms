bd79ff1a7128043d0b883065ddb9346a110f2490
Roland <ich@rolandkaercher.de>
2016-11-11 07:29:49 +0100

    Version 0.4.25

--- components/timbre-logstash/project.clj
+++ components/timbre-logstash/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/timbre-logstash "0.4.25-SNAPSHOT"
+(defproject gorillalabs.tesla/timbre-logstash "0.4.25"
             :plugins [[lein-modules "0.3.11"]]
             :modules {:parent "../.."}
             :description "A logastash component"
