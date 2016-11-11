6b2210bba7fa0a76f2874c1cf1d41fabc9cb15f5
Roland <ich@rolandkaercher.de>
2016-11-11 07:30:38 +0100

    Version 0.4.26-SNAPSHOT

--- components/quartzite/project.clj
+++ components/quartzite/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/quartzite "0.4.25"
+(defproject gorillalabs.tesla/quartzite "0.4.26-SNAPSHOT"
             :plugins [[lein-modules "0.3.11"]]
             :modules {:parent "../.."}
             :description "A quartzite component"
