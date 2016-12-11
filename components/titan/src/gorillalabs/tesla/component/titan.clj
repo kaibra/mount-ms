63c359211dcc24cffc45610a5ec5d69d45bdccee
Christoph <git@xrstf.de>
2016-12-11 19:34:50 +0100

    log the storage and index hostname when starting the titan component

--- components/titan/src/gorillalabs/tesla/component/titan.clj
+++ components/titan/src/gorillalabs/tesla/component/titan.clj
@@ -1,23 +1,23 @@
 (ns gorillalabs.tesla.component.titan
-  (:require
-   [clojurewerkz.titanium.graph :as tg]
-   [clojure.tools.logging :as log]
-   [gorillalabs.tesla.component.configuration :as config]
-   [mount.core :as mnt]))
+  (:require [clojurewerkz.titanium.graph :as tg]
+            [clojure.tools.logging :as log]
+            [gorillalabs.tesla.component.configuration :as config]
+            [mount.core :as mnt]))
 
 (defn titan-config [config]
-  (merge {"storage.backend"  "cassandrathrift"
-          "index.search.backend" "lucene"
-          "index.search.directory" "/tmp/searchIndex"
-          "attributes.custom.attribute1.attribute-class" "clojure.lang.Keyword"
+  (merge {"storage.backend"                               "cassandrathrift"
+          "index.search.backend"                          "lucene"
+          "index.search.directory"                        "/tmp/searchIndex"
+          "attributes.custom.attribute1.attribute-class"  "clojure.lang.Keyword"
           "attributes.custom.attribute1.serializer-class" "hamburg.cypp.KeywordSerializer"}
          config))
 
 (defn- start []
-  (log/info "-> starting titan")
-  (let [server-config (titan-config (:titan config/configuration))
-        graph (tg/open server-config)]
-    graph))
+  (let [server-config (titan-config (:titan config/configuration))]
+    (log/infof "-> starting titan (storage @ %s, search index @ %s)"
+               (get server-config "storage.hostname" "[not set]")
+               (get server-config "index.search.hostname" "[not set]"))
+    (tg/open server-config)))
 
 (defn- stop [graph]
   (if graph
@@ -26,8 +26,6 @@
       (tg/close graph))
     (log/info "<- titan not running")))
 
-
 (mnt/defstate graph
               :start (start)
               :stop (stop graph))
-
