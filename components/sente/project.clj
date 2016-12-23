f83c844ac3db3ae46b03dfc92b0deba4a10db64c
Christoph <git@xrstf.de>
2016-12-23 11:50:36 +0100

    Version 0.4.35

--- components/sente/project.clj
+++ components/sente/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/sente "0.4.35-SNAPSHOT"
+(defproject gorillalabs.tesla/sente "0.4.35"
             :plugins [[lein-modules "0.3.11"]]
             :modules {:parent "../.."}
             :description "sente websocket component"
