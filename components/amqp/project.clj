134a79b84210bb08e85b609be61fc5384d55a2b4
Jens Lehnhoff <mail@jenslehnhoff.de>
2017-01-10 11:00:20 +0100

    Version 0.4.38-SNAPSHOT

--- components/amqp/project.clj
+++ components/amqp/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/amqp "0.4.37"
+(defproject gorillalabs.tesla/amqp "0.4.38-SNAPSHOT"
             :plugins [[lein-modules "0.3.11"]]
             :modules {:parent "../.."}
             :description "A component to connect to amqp queues and exchanges."
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs/tesla "0.4.37"
+(defproject gorillalabs/tesla "0.4.38-SNAPSHOT"
   :description "basic microservice."
   :plugins [[lein-modules "0.3.11"]
             [lein-pprint "1.1.1"]]
