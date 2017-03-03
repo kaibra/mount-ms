4d9ab19d9f645079390295c7cb4842d978e568d1
Roland <ich@rolandkaercher.de>
2017-03-03 13:12:17 +0100

    Version 0.4.45-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs/tesla "0.4.44"
+(defproject gorillalabs/tesla "0.4.45-SNAPSHOT"
   :description "basic microservice."
   :plugins [[lein-modules "0.3.11"]
             [lein-pprint "1.1.1"]]
