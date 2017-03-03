cd921bb8045ba6a471d6375008a964cfce9c3160
Roland <ich@rolandkaercher.de>
2017-03-03 13:26:07 +0100

    Fixed version numbers.

--- components/quartzite/project.clj
+++ components/quartzite/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/quartzite "0.4.40-SNAPSHOT"
+(defproject gorillalabs.tesla/quartzite "0.4.48-SNAPSHOT"
             :plugins [[lein-modules "0.3.11"]]
             :modules {:parent "../.."}
             :description "A quartzite component"
