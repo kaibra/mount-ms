c1109475f394ec8d8b3dedff92305e5caf7d99a4
Roland <ich@rolandkaercher.de>
2017-03-14 14:26:36 +0100

    Version 0.4.49

--- components/amqp/project.clj
+++ components/amqp/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/amqp "0.4.49-SNAPSHOT"
+(defproject gorillalabs.tesla/amqp "0.4.49"
             :plugins [[lein-modules "0.3.11"]]
             :modules {:parent "../.."}
             :description "A component to connect to amqp queues and exchanges."
--- components/sente/project.clj
+++ components/sente/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/sente "0.4.49-SNAPSHOT"
+(defproject gorillalabs.tesla/sente "0.4.49"
             :plugins [[lein-modules "0.3.11"]]
             :modules {:parent "../.."}
             :description "sente websocket component"
