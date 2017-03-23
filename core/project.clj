54d9d371ba032567359958ed59d3d6ce5dab52df
Christoph <git@xrstf.de>
2017-03-23 20:09:07 +0100

    Version 0.4.50

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/core "0.4.50-SNAPSHOT"
+(defproject gorillalabs.tesla/core "0.4.50"
             :plugins [[lein-modules "0.3.11"]]
             :description "basic microservice."
             :dependencies [[mount "0.1.10"]
