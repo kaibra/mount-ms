b451c293c29d0eb5c1a51c36ac96223e7e4cdaee
Christoph <git@xrstf.de>
2017-03-24 13:29:11 +0100

    Version 0.4.52

--- components/mongo/project.clj
+++ components/mongo/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/mongo "0.4.52-SNAPSHOT"
+(defproject gorillalabs.tesla/mongo "0.4.52"
             :plugins [[lein-modules "0.3.11"]]
             :modules {:parent "../.."}
             :description "A mongo component"
--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/core "0.4.52-SNAPSHOT"
+(defproject gorillalabs.tesla/core "0.4.52"
             :plugins [[lein-modules "0.3.11"]]
             :description "basic microservice."
             :dependencies [[mount "0.1.10"]
