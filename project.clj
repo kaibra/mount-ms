9fc94f9a9c54f2a3d868dd5adc4cd4a86bc31fd7
Christoph <git@xrstf.de>
2017-05-05 11:34:53 +0200

    Version 0.4.55

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs/tesla "0.4.55-SNAPSHOT"
+(defproject gorillalabs/tesla "0.4.55"
   :description "basic microservice."
   :plugins [[lein-modules "0.3.11"]
             [lein-pprint "1.1.1"]]
